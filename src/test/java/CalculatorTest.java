f2c3be53e6fca7473086b889f17049faf044f866
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-07-20 14:56:16 +0100

    Initial commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calculator;
+
+    @Before
+    public void before(){
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void canAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+//
+//    @Test
+//    public void canSubtract() {
+//        assertEquals(1, calculator.subtract(4, 3));
+//    }
+//
+//    @Test
+//    public void canMultiply() {
+//        assertEquals(15, calculator.multiply(3, 5));
+//    }
+//
+//    @Test
+//    public void canDivide() {
+//        assertEquals(2.5, calculator.divide(5, 2));
+//    }
+}
